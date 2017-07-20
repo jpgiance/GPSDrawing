f02c6feb7678218c8c32ffb095c96d8341fe6edf
jpgiance <jpgiance@gmail.com>
2017-07-20 00:42:28 -0500

    simple drawing app with some details to improve

---
+++ app/src/main/java/com/example/jorge/gpsdrawing/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.jorge.gpsdrawing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CanvasView canvasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        canvasView = (CanvasView) findViewById(R.id.canvas);
+    }
+
+    public void clearCanvas(View v) {
+        canvasView.clearCanvas();
+
+    }
+}
